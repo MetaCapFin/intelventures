1ad4f526b37c9fe0130585e3b170335b53ae82a3
MetaCapFin <advisor@metacap.finance>
2025-04-25 13:04:13 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</meta>
+
 import { useState, useEffect, useCallback, ReactNode, useMemo } from 'react';
 import Image from 'next/image';
 
